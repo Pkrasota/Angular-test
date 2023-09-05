ae16d01dabfb7bb558ece4783674dfdaa488e0c5
Pkrasota <pavlokrasota@gmail.com>
2023-09-05 15:02:47 +0300

    aded own demo observable

---
+++ src/rxjs-demo.ts
@@ -0,0 +1,27 @@
+import {debounce, debounceTime, distinctUntilChanged, map, Observable} from "rxjs";
+
+const search$ = new Observable<Event>(observer => {
+  const search = document.getElementById('search');
+  if(!search) {
+    observer.error('Element does not not exist')
+    return;
+  }
+  search.addEventListener('input', event => {
+    observer.next(event)
+  });
+ });
+
+search$.pipe(
+  map(event => {
+    return (event.target as HTMLInputElement).value;
+  }),
+    debounceTime(500),
+    map(value => value.length > 3 ? value : ''),
+    distinctUntilChanged()
+).subscribe(value => {
+  console.log(value)
+})
+
+
+
+
